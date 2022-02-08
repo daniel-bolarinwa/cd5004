817ab702c3419a93732cf0b2f5570a322e39c9fa
Daniel Bolarinwa <dtireni@gmaail.com>
2022-02-08 17:03:03 +0000

    added bank account tasks

--- week2/TestResetable.java
+++ week2/TestResetable.java
@@ -1,10 +1,16 @@
 public class TestResetable {
     public static void main (String[] args) {
+        BankAccount testAccount = new BankAccount("122222122", "Daniel");
+        testAccount.deposit(550);
         Oblong testOblong = new Oblong (8,8); 
         System.out.println("resetting oblong height and length from current values " + testOblong.getHeight() + " " + testOblong.getLength());
         resetObject(() ->{
             testOblong.setHeight(1);
             testOblong.setLength(1);
+        });
+
+        resetObject(() ->{
+            testAccount.withdraw(testAccount.getBalance());
         });
 
         System.out.println("oblong height and length after reset is now " + testOblong.getHeight() + " " + testOblong.getLength());
