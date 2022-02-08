a81ead40ac3b4cc287fc9dfd503ce1c962459bc7
Daniel Bolarinwa <dtireni@gmaail.com>
2022-02-08 17:09:33 +0000

    improved logging messages

--- week2/TestResetable.java
+++ week2/TestResetable.java
@@ -2,9 +2,9 @@ public class TestResetable {
     public static void main (String[] args) {
         BankAccount testAccount = new BankAccount("122222122", "Daniel");
         testAccount.deposit(550);
-        System.out.println("current bank account balance for " + testAccount.getAccountName() + " is" + testAccount.getBalance());
+        System.out.println("current bank account balance for " + testAccount.getAccountName() + " is " + testAccount.getBalance());
         Oblong testOblong = new Oblong (8,8); 
-        System.out.println("oblong height and length current values are height: " + testOblong.getHeight() + " and length: " + testOblong.getLength());
+        System.out.println("oblong current measurements are height: " + testOblong.getHeight() + " and length: " + testOblong.getLength());
         resetObject(() ->{
             testOblong.setHeight(1);
             testOblong.setLength(1);
@@ -14,8 +14,8 @@ public class TestResetable {
             testAccount.withdraw(testAccount.getBalance());
         });
 
-        System.out.println("oblong height and length after resetting is now height: " + testOblong.getHeight() + " and length: " + testOblong.getLength());
-        System.out.println("current bank account balance for " + testAccount.getAccountName() + " is" + testAccount.getBalance() + " after resetting the account balance");
+        System.out.println("oblong measurements after resetting are height: " + testOblong.getHeight() + " and length: " + testOblong.getLength());
+        System.out.println("current bank account balance for " + testAccount.getAccountName() + " is " + testAccount.getBalance() + " after resetting the account balance");
     }
 
     public static void resetObject(Resetable objectIn) {
