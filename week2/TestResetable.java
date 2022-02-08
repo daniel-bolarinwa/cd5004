59db138428d0a6b720902bbcb0aac376d3a46d61
Daniel Bolarinwa <dtireni@gmaail.com>
2022-02-08 17:06:54 +0000

    added proper logging

--- week2/TestResetable.java
+++ week2/TestResetable.java
@@ -2,8 +2,9 @@ public class TestResetable {
     public static void main (String[] args) {
         BankAccount testAccount = new BankAccount("122222122", "Daniel");
         testAccount.deposit(550);
+        System.out.println("current bank account balance for " + testAccount.getAccountName() + " is" + testAccount.getBalance());
         Oblong testOblong = new Oblong (8,8); 
-        System.out.println("resetting oblong height and length from current values " + testOblong.getHeight() + " " + testOblong.getLength());
+        System.out.println("oblong height and length current values are height: " + testOblong.getHeight() + " and length: " + testOblong.getLength());
         resetObject(() ->{
             testOblong.setHeight(1);
             testOblong.setLength(1);
@@ -13,7 +14,8 @@ public class TestResetable {
             testAccount.withdraw(testAccount.getBalance());
         });
 
-        System.out.println("oblong height and length after reset is now " + testOblong.getHeight() + " " + testOblong.getLength());
+        System.out.println("oblong height and length after resetting is now height: " + testOblong.getHeight() + " and length: " + testOblong.getLength());
+        System.out.println("current bank account balance for " + testAccount.getAccountName() + " is" + testAccount.getBalance() + " after resetting the account balance");
     }
 
     public static void resetObject(Resetable objectIn) {
